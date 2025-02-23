5e23354c0d280a53560819d9b6af08bb3c5da0d8
kconners-sec <kconners@securustechnologies.com>
2025-02-23 08:48:38 -0600

    Work work work.

---
+++ tests/cypress/e2e/ui/CustomerManagement.cy.ts
@@ -0,0 +1,9 @@
+describe('Customer Management Page', () => {
+    before(() => {
+        cy.visit('/')
+      })
+    //cy.visit('https://example.cypress.io/')
+    it('Will have a label', () => {
+        cy.log("Hey Kaylen")
+    })
+})
